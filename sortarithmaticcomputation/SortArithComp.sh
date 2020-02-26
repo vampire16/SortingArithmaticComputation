59a453dc16f95f89dd1cbf741822ecd13649f40a
vampire16 <siddheshsalvi525@gmai.com>
2020-02-26 10:00:59 +0530

    ADDED : Reading inputs from user

--- sortarithmaticcomputation/SortArithComp.sh
+++ sortarithmaticcomputation/SortArithComp.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
 echo "Sorting arithmatic computation"
+
+#READING THREE INPUTS FROM USER
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c
+
+echo "$a ,$b ,$c "
